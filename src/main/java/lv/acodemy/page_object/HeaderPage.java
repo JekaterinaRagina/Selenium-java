7ad20f05dc9b81646de124f92e11198287bec26b
Ragina <ragina.j@inbox.lv>
2025-05-05 20:56:21 +0300

    [master]: Add progress

---
+++ src/main/java/lv/acodemy/page_object/HeaderPage.java
@@ -0,0 +1,26 @@
+package lv.acodemy.page_object;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HeaderPage {
+    public HeaderPage(ChromeDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(className = "shopping_cart_badge")
+    public WebElement cartBadge;
+
+    @FindBy(className = "shopping_cart_link")
+    public WebElement shoppingCartLink;
+
+    public String getCartBadgeText() {
+        return cartBadge.getText();
+    }
+
+    public WebElement getShoppingCartLink() {
+        return shoppingCartLink;
+    }
+}
--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -1,29 +1,56 @@
-import lv.acodemy.page_object.InventoryPage;
-import lv.acodemy.page_object.LoginPage;
+import com.github.javafaker.Faker;
+import lv.acodemy.page_object.*;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.Assertion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SauceDemoTest {
 
     ChromeDriver driver;
+    ChromeOptions options;
+
     LoginPage loginPage;
     InventoryPage inventoryPage;
+    HeaderPage headerPage;
+    CartPage cartPage;
+    CheckoutPage checkoutPage;
+
+    private static final Logger logger = LoggerFactory.getLogger(SauceDemoTest.class);
+
+    Faker data = new Faker();
 
 
     @BeforeMethod
     public void beforeTest() {
 
-        driver = new ChromeDriver();
+        options = new ChromeOptions();
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        options.setExperimentalOption("prefs", prefs);
+
+        options.addArguments("--incognito");
+
+        driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com");
 
         loginPage = new LoginPage(driver);
         inventoryPage = new InventoryPage(driver);
+        headerPage = new HeaderPage(driver);
+        cartPage = new CartPage(driver);
+        checkoutPage = new CheckoutPage(driver);
 
     }
 
@@ -52,9 +79,18 @@ public class SauceDemoTest {
     @Test
     public void addItemToCartTest() {
         loginPage.authorize("standard_user", "secret_sauce");
-
         InventoryPage inventoryPage = new InventoryPage(driver);
         inventoryPage.addItemToCartByName("Onesie");
+        Assertions.assertThat(headerPage.getCartBadgeText()).isEqualTo("1");
+
+        headerPage.getShoppingCartLink().click();
+        Assertions.assertThat(cartPage.getCartItems().size()).isEqualTo("1");
+
+        cartPage.getCheckoutButton().click();
+
+        String firstName = data.name().firstName();
+        String lastName = data.name().lastName();
+        String postalCode = data.address().zipCode();
     }
 
     @AfterMethod()
